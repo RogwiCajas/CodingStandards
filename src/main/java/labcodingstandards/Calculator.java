dfda72b0d1a53fda017e6c3ce5d0c1aff068abe2
RogwiCajas <rogwinalex2@hotmail.com>
2021-06-04 16:13:42 -0500

    resolviendo otros errores por hobby

--- src/main/java/labcodingstandards/Calculator.java
+++ src/main/java/labcodingstandards/Calculator.java
@@ -14,71 +14,66 @@ public class Calculator {
 	 * Main method.
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		Scanner reader = new Scanner(System.in);
         System.out.print("1. +\n2. -\n3. *\n4. /\nEnter an operator: ");
         char operator = reader.nextLine().charAt(0);
         double first;
         double second;
         String input;
-        
         while (true) {
         	System.out.print("Enter first number: ");
         	input = reader.nextLine();
-        	
             try {
-            	First=Integer.parseInt(input);
+            	first = Integer.parseInt(input);
                 break;
             } catch (NumberFormatException e) {
                 System.out.println("Not valid!");
             }
         }
-        
         while (true) {
         	System.out.print("Enter second number: ");
         	input = reader.nextLine();
-        	
             try {
-            	second=Integer.parseInt(input);
+            	second = Integer.parseInt(input);
                 break;
             } catch (NumberFormatException e) {
                 System.out.println("Not valid!");
             }
         }
 
-        Calculator cal=new Calculator();
-        String result=cal.Operation(first, second, operator);
+        Calculator cal = new Calculator();
+        String result = cal.operation(first, second, operator);
 
         System.out.printf(result);
 		reader.close();
 	}
 	//CHECKSTYLE:ON
 	/**
-	 * This method makes an operation
+	 * This method makes an operation.
 	 * @param first
 	 * @param second
 	 * @param operator
-	 * @return
+	 * @return String
 	 */
-	private String operation (double first, double second, char operator) {
+	private String operation(final double first, final double second, final char operator){
 		double result = 0;
-		switch(operator)
-        {
+		switch (operator) {
             case '1':
-                result=first+second;
+                result = first + second;
                 break;
             case '2':
-                result=first-second;
+                result = first - second;
                 break;
             case '3':
-                result=first*second;
+                result = first * second;
                 break;
             case '4':
-                result =first/second;
+                result = first / second;
                 break;
             default:
             	return "Error! operator is not correct";
         }
-		return "The result is: "+result;
+		return "The result is: " + result;
 	}
 }
